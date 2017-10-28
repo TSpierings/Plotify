6cb2468cd1511584c24f10c4529eee461aca1060
Tchaka Spierings <tchaka.spierings@gmail.com>
2017-10-28 23:04:42 +0200

    Add 'load more' button

--- src/app/modules/top-artists/top-artists/top-artists.component.ts
+++ src/app/modules/top-artists/top-artists/top-artists.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { TopArtists, Item } from '../../interfaces/artists';
 
 @Component({
@@ -11,20 +11,30 @@ export class TopArtistsComponent implements OnInit {
 
   topArtists: TopArtists;
 
+  private offset: number = 0;
+
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getTop();
   }
 
-  private getTop() {
+  getTop() {
     const token = localStorage.getItem('access_token');
     const header = new HttpHeaders({'Authorization': 'Bearer ' + token});
     const url = 'https://api.spotify.com/v1/me/top/artists';
-    this.http.get(url, { headers: header}).toPromise()
+    let params = new HttpParams().set('offset', this.offset.toString());
+
+    this.http.get(url, { headers: header, params: params}).toPromise()
       .then(data => {
-        console.log(data);
-        this.topArtists = data as TopArtists;
+        this.offset += 20;
+        const newArtists = data as TopArtists;
+
+        if (this.topArtists == null) {
+          this.topArtists = newArtists;
+        }
+
+        this.topArtists.items.push(...newArtists.items);     
       })
       .catch(error => console.log(error));
   }
