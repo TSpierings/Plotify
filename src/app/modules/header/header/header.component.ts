7cbe6d7fed4febabf7b549e1bf7054e32b8a7cf7
Tchaka Spierings <tchaka.spierings@gmail.com>
2017-10-29 18:47:01 +0100

    Remove debug console log

--- src/app/modules/header/header/header.component.ts
+++ src/app/modules/header/header/header.component.ts
@@ -17,7 +17,6 @@ export class HeaderComponent implements OnInit {
 
   @HostListener('document:click', ['$event'])
   closeDropDown(event): void {
-    console.log(event.target.classList);
     if (!event.target.classList.contains('hampart')) {
       this.open = false;
     }
