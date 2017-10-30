bacd46b08ed74c389f7327eeae6799e9ded3823d
Tchaka Spierings <tchaka.spierings@gmail.com>
2017-10-30 09:56:09 +0100

    Animate artist list

--- src/app/modules/top-artists/artist-list/artist-list.component.ts
+++ src/app/modules/top-artists/artist-list/artist-list.component.ts
@@ -1,10 +1,35 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Item }  from '../../interfaces/artists'
+import {
+  trigger,
+  style,
+  animate,
+  transition,
+  stagger,
+  query
+} from '@angular/animations';
 
 @Component({
   selector: 'app-artist-list',
   templateUrl: './artist-list.component.html',
-  styleUrls: ['./artist-list.component.scss']
+  styleUrls: ['./artist-list.component.scss'],
+  animations: [
+    trigger('cardstate', [
+      transition('* => *', [
+        query('.card:enter', style({ opacity: '0' }), { optional: true }),
+        query('.card:enter', [
+          stagger(50, [
+            style({ opacity: '0' }),
+            animate(250, style({ opacity: '1' }))
+          ])
+        ], { optional: true }),
+        query('.card:leave', [
+          style({ opacity: '1' }),
+          animate(250, style({ opacity: '0' }))
+        ], { optional: true }),
+      ])
+    ])
+  ]
 })
 export class ArtistListComponent implements OnInit {
 
