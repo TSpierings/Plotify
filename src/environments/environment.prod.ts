950134e41d06b8d72e0cf67257b5c80f24f9e1e2
Tchaka Spierings <tchaka.spierings@gmail.com>
2017-11-18 12:50:50 +0100

    Change callback url to plotify.net

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   clientID: 'a4984ffdc4f140d39634d0e071664ffe',
-  callbackURL: 'https://plotify-app.firebaseapp.com'
+  callbackURL: 'https://plotify.net'
 };
